1f334d43c6303027eac1793fe2e62f1d783cf38f
Koteswarrao375 <koteswar.meesala.ext@siemens.com>
2019-10-24 00:22:38 +0530

    reversed commit

--- src/AlphabetInvasionGame.js
+++ src/AlphabetInvasionGame.js
@@ -1,7 +1,12 @@
 export const message = "Hello there";
 
 const first = of("first");
+<<<<<<< HEAD
 const second = of("second");
 const third = of("third");
+=======
+const first = of("first");
+const first = of("first");
+>>>>>>> parent of c0297a6... first
 
 
