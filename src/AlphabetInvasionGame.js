66b978c83a193056edf57c8334dc6e1f8b4222bd
Koteswar Rao Meesala <koteswar.meesala.ext@siemens.com>
2019-10-23 15:30:31 +0530

    reverted changes

--- src/AlphabetInvasionGame.js
+++ src/AlphabetInvasionGame.js
@@ -1,6 +1,9 @@
 export const message = "Hello there";
 
 const first = of("first");
+<<<<<<< HEAD
 const second = of("second");
 const third = of("third");
 const fourth = of("fourth");
+=======
+>>>>>>> parent of 9593f00... second commit
