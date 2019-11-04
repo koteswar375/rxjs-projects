121307aae70b954d8362cd6a166924e6782b399f
Koteswar Rao Meesala <koteswar.meesala.ext@siemens.com>
2019-11-04 12:12:31 +0530

    First commit

--- src/AlphabetInvasionGame.js
+++ src/AlphabetInvasionGame.js
@@ -1,12 +1,10 @@
 export const message = "Hello there";
 
 const first = of("first");
-<<<<<<< HEAD
 const second = of("second");
 const third = of("third");
-=======
 const first = of("first");
 const first = of("first");
->>>>>>> parent of c0297a6... first
 
 
+const line = "This is added in the first commit";
