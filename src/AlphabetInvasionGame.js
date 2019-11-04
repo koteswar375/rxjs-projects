90c7e2d1bbbbc83ae2044e62aaf538f883c536be
Koteswar Rao Meesala <koteswar.meesala.ext@siemens.com>
2019-11-04 12:25:56 +0530

    Changes accepted

--- src/AlphabetInvasionGame.js
+++ src/AlphabetInvasionGame.js
@@ -1,18 +1,7 @@
 export const message = "Hello there";
 
 const first = of("first");
-<<<<<<< HEAD
-const second = of("second");
-const third = of("third");
-=======
 const first = of("first");
 const first = of("first");
->>>>>>> parent of c0297a6... first
 
 
-<<<<<<< HEAD
-const line = "This is added in the first commit";
-const line2 = "This is added in the second commit";
-const line3 = "This is the most recent commit";
-=======
->>>>>>> parent of 121307a... First commit
